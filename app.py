161201cced406c3820d3bbd47fde78b03e104dd0
Hisanori Nara <87920607+HissaNara@users.noreply.github.com>
2025-08-25 21:51:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,3 @@
-Skip to content
-Navigation Menu
-HissaNara
-Crypto-price-checker-in-the-console
-
-Type / to search
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-Comparing changes
-Choose two branches to see what’s changed or to start a new pull request. If you need to, you can also  or learn more about diff comparisons.
- 
-...
- 
- 1 commit
- 1 file changed
- 1 contributor
-Commits on Aug 25, 2025
-Update app.py
-
-@HissaNara
-HissaNara authored 15 minutes ago
- Showing  with 11 additions and 24 deletions.
- 35 changes: 11 additions & 24 deletions35  
-app.py
-Original file line number	Diff line number	Diff line change
-@@ -1,35 +1,22 @@
 from flask import Flask,jsonify,render_template
 import requests
 import time
@@ -77,15 +44,3 @@ if __name__ == "__main__":
     t.start() 
 
     app.run(debug=False)
-Footer
-© 2025 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact
-Manage cookies
-Do not share my personal information
-Comparing 073289aec75f990cdf67f89b03dfc272aa43a834...9a93fd5c553e3d0763fb769c2851fce89a59a024 · HissaNara/Crypto-price-checker-in-the-console
